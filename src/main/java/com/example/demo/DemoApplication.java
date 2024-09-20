a69f2ad45f410d6866681c44475ea4c8aab227e0
Guilherme Silvestre Giazzi <guilherme.giazzi@sysmap.com.br>
2024-09-19 21:38:42 -0300

    working app deployed to azure spring apps

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.microsoft.azure.samples.springcredentialfree;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCredentialFreeApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCredentialFreeApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
