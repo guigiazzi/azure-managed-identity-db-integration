df6992c116a25e4f3698f7365c09832537058388
Guilherme Silvestre Giazzi <guilherme.giazzi@sysmap.com.br>
2024-09-24 15:04:04 -0300

    WIP: managed identity integration

--- src/main/java/com/azure/cosmos/samples/mongodbspring/ProductRepository.java
+++ src/main/java/com/azure/cosmos/samples/mongodbspring/ProductRepository.java
@@ -1,16 +1,16 @@
-package com.azure.cosmos.samples.mongodbspring;
+// package com.azure.cosmos.samples.mongodbspring;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.repository.MongoRepository;
+// import org.springframework.data.mongodb.core.mapping.Document;
+// import org.springframework.data.mongodb.repository.MongoRepository;
 
-@Document(collection = "products")
-public interface ProductRepository extends MongoRepository<Product, String> {
+// @Document(collection = "products")
+// public interface ProductRepository extends MongoRepository<Product, String> {
 
-    public Optional<Product> findById(String id);
+//     public Optional<Product> findById(String id);
 
-    public List<Product> findByCategory(String category);
+//     public List<Product> findByCategory(String category);
     
-}
+// }
